8e965c6c9940d5cceff895643ee41599d07b2ab6
Yu Pian <854414102@qq.com>
2020-05-09 21:35:29 +0800

    11111111 测试提交

--- demo-client/src/main/java/com/example/demo/DemoApplication.java
+++ demo-client/src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("DemoApplication.class");
         SpringApplication.run(DemoApplication.class, args);
     }
 
